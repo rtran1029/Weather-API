c9fceaa52916ca7f89a5db57a1c73e77adf462b8
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-07-03 21:32:39 -0400

    script edits

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
 let apiKey = "e4e2bcc1930c01d116429662a137d18d";
 let savedSearches = [];
 
+var searchHistoryList = function(cityName) {
+    $('.past-search:contains("' + cityName + '")').remove();
+
+    var searchHistoryEntry = $("<p>");
+    searchHistoryEntry.addClass("past-search");
+    searchHistoryEntry.text(cityName);
+
+    var searchEntryContainer = $("<div>");
+    searchEntryContainer.addClass("past-search-container");
+
+    searchEntryContainer.append(searchHistoryEntry);
+
+    var searchHistoryContainerEl = $("#search-history-container");
+    searchHistoryContainerEl.append(searchEntryContainer);
+
+    if (savedSearches.length > 0){
+        var previousSavedSearches = localStorage.getItem("savedSearches");
+        savedSearches = JSON.parse(previousSavedSearches);
+    }
+
+    savedSearches.push(cityName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+
+    $("#search-input").val("");
+
+};
+
 $("#search-form").on("submit", function() {
     event.preventDefault();
     
-    // get name of city searched
     var cityName = $("#search-input").val();
 
     if (cityName === "" || cityName == null) {
-        //send alert if search input is empty when submitted
         alert("Please enter name of city.");
         event.preventDefault();
     } else {
-        // if cityName is valid, add it to search history list and display its weather conditions
         currentWeatherSection(cityName);
         fiveDayForecastSection(cityName);
     }
